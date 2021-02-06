c4e7b687bded8e6117a633ec3105f241a7a3e4fb
Zerui-WANG <71635569+Zerui-WANG@users.noreply.github.com>
2021-02-06 21:56:31 +0100

    'second commit'

--- rOpenExo/urls.py
+++ rOpenExo/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
